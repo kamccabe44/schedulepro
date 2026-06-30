f284c6c56622d9445a039a045342982998e97de0
Kevin McCabe <k.a.mccabe.44@gmail.com>
2026-06-30 14:05:31 -0600

    updated handlers

--- backend/handlers.go
+++ backend/handlers.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/dynamodb/types"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
